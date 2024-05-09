6fd210f1d92f029f06a70115cad7bcc6700abcae
User <glennvrumpt@outlook.com>
2024-05-09 15:30:45 +0200

    Update project

--- src/components/transform.js
+++ src/components/transform.js
@@ -5,11 +5,13 @@ class Transform extends Component {
   constructor(
     position = new Vector2(0, 0),
     velocity = new Vector2(0, 0),
+    scale = new Vector2(1, 1),
     angle = 0
   ) {
     super();
     this.position = position;
     this.velocity = velocity;
+    this.scale = scale;
     this.angle = angle;
   }
 }
